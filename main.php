285c93c993290638a402ba6609ca4df21081286f
lepoivre.laurent@gmail.com <lepoivre.laurent@gmail.com>
2023-08-21 14:42:11 +0200

    switch case

---
+++ main.php
@@ -0,0 +1,35 @@
+<?php
+include_once("./banque./agence.php");
+include_once("./banque./client.php");
+include_once("./banque./compte.php");
+
+$chaine_menu ="";
+switch ($choix_menu) {
+    case '1':
+        //créer une agence
+        break;
+    case '2':
+        //créer un client
+        break;
+    case '3':
+        //créer un compte bancaire
+        break;
+    case '4':
+        // recherche de compte
+        break;
+    case '5':
+        // recherche de client
+        break;
+    case '6':
+        //afficher la liste des comptes d'un client
+        break;
+    case '7':
+        //imprimer les infos clients
+        break;
+    case '8':
+        //quitter le programme
+        break;
+    default:
+        
+        break;
+}
