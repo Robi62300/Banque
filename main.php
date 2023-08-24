cd4ccbf47ceecec6063b15a7fecf48a0ac1b2aee
lepoivre.laurent@gmail.com <lepoivre.laurent@gmail.com>
2023-08-24 15:16:49 +0200

    qqs modifications

--- main.php
+++ main.php
@@ -7,33 +7,37 @@ include_once("./lib/function.php");
 
 $chaine_menu ="Bienvenue chez DWWM-02, veuillez choisir votre choix : ";
 $choix_menu= readline($chaine_menu);
+do {
 switch ($choix_menu) {
     case '1': 
+        $donnees = [];
         $agence = new Agence();
         $agence ->setCodeAgence();
         $agence ->setNomAgence();
-        $agence->setAdresse();
+        array_push($donnees, $agence->setAdresse());
         $fileName = "./sauv/agences/agences.csv";
         var_dump($agence);
         arrayToCsv($agence, $fileName, ",");
         break;
     case '2':
+        $donnees = [];
         $client= new Client();
         $client->setIdClient();
         $client->setNom();
         $client->setPrenom();
         $client->setDateNaissance();
-        $client->setEmail();
-        arrayToCsv($client, "./sauv/clients/clients.csv", ",");
-        print_r($client);
+        array_push($donnees, $client->setEmail());
+        arrayToCsv($donnees, "./sauv/clients/clients.csv", ",");
+        print_r($donnees);
         break;
     case '3':
+       $donnees = [];
        $compte = new Comptes();
        $compte ->setNumeroCompte();
        $compte ->setTypeCompte();
        $compte ->setSolde();
-       $compte ->setDecouvertAutorise();
-
+       array_push($donnees, $compte ->setDecouvertAutorise());
+       arrayToCsv($donnees, "./sauv/comptes/comptes.csv", ",");
         break;
     case '4':
         // recherche de compte
@@ -49,12 +53,14 @@ switch ($choix_menu) {
         break;
     case '8':
         //quitter le programme
+        $choix_menu =10;
         break;
     default:
         echo("erreur de saisie dans le menu");
+        $choix_menu=10;
         break;
 }
-
+}while ($choix_menu<10);
 // $nom=new Client("toto", "tata", "uiyefuif", "hfufh", "ygeagyzg");
 // $nom->setNom();
 // $agence = new agence ();
@@ -64,3 +70,23 @@ switch ($choix_menu) {
 //rand(10000000000, 99999999999); génère un nombre de 11 chiffres
 //rand(100000, 999999); 6 chiffres
 //rand(100, 999); 3 chiffres 
+
+//masque pour la date de naissance
+/*
+$valid_format_datenaiss = "#(\d{2})/(\d{2})/(\d{4})#";
+if(!preg_match($valide_format_datenaiss, chaine_date)) //ici chaine_date est la chaine de caractères qu'on rentre
+{
+    echo renter une date valide
+}
+*/
+
+//masque pour un e-mail
+/*
+ $masque = "/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}$/";
+
+if(preg_match($masque, $email)) 
+ {
+        mail valide
+ }
+ mail non valide
+*/
